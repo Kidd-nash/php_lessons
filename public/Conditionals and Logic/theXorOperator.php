72c409ead7c2b08fa6920b833fbc983c38b9e94f
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-03 11:26:35 +0800

    the xor operator

---
+++ public/Conditionals and Logic/theXorOperator.php
@@ -0,0 +1,17 @@
+<?php
+
+$fruit = "banana";
+$protein = "pork";
+
+// Write your code below:
+if ($fruit == "banana" xor $protein == "chicken"){
+  echo "Dig in!";
+} 
+
+// TRUE xor TRUE;    Evaluates to: FALSE
+
+// FALSE xor TRUE;   Evaluates to: TRUE
+
+// TRUE xor FALSE;   Evaluates to: TRUE
+
+// FALSE xor FALSE;  Evaluates to: FALSE
