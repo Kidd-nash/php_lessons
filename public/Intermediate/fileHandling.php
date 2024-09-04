325af5fd792ad333a8628d24bb6370152adeb2cd
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-04 11:41:03 +0800

    reading a file functions and proper procedure

--- public/Intermediate/fileHandling.php
+++ public/Intermediate/fileHandling.php
@@ -4,3 +4,14 @@
 $info_file = fopen("info.txt", "r");
 //closes file
 fclose($info_file);
+
+//open file in read mode and stored in the variable
+$welcome_file = fopen("welcome.txt", "r");
+//file size to read
+$size = filesize("welcome.txt");
+//function read the context of wilcome text
+$welcome_text = fread($welcome_file, $size);
+//echo to find out what is in the file
+echo $welcome_text;
+//then close once done
+fclose($welcome_file);
