39090ba19593d9f4569b242fde98fc3ce07cc708
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-04 12:10:19 +0800

    file information operation

--- public/Intermediate/fileHandling.php
+++ public/Intermediate/fileHandling.php
@@ -26,12 +26,14 @@ fwrite($bands_file, "Still Woozy");
 //check out the file then close...
 fclose($bands_file);
 
+
 //open file in append mode
 $pets_file = fopen("cool-pets.txt", "a");
 //add favorite pet name to the file
 fwrite($pets_file, "Funcho"); // varies
 //check out the file then close...
 fclose($pets_file);
+
 
 // Long version of reading a text and appending contents
 // $read_file = fopen("read-me.txt", "r");
@@ -47,3 +49,15 @@ fclose($pets_file);
 $read_text = file_get_contents("read-me.txt");
 
 file_put_contents("append-me.txt", "I love PHP!", FILE_APPEND);
+
+
+//initially set equal to hey.text and file does not exist
+$name = "hello.txt";
+//create a variable that calls the file_exists on the variable name
+$exists = file_exists($name);
+//create an if statement to read the file contents, else echo file not found
+if ($exists) {
+  echo file_get_contents($name);
+} else {
+  echo "file not found";
+}
