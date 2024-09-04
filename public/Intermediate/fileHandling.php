42495b7ed59266b9cc337847130d9923e3c1cc1e
Kidd-nash <arkady.pantaleon@outlook.com>
2024-09-04 11:54:16 +0800

    writing to a file

--- public/Intermediate/fileHandling.php
+++ public/Intermediate/fileHandling.php
@@ -5,6 +5,8 @@ $info_file = fopen("info.txt", "r");
 //closes file
 fclose($info_file);
 
+
+
 //open file in read mode and stored in the variable
 $welcome_file = fopen("welcome.txt", "r");
 //file size to read
@@ -15,3 +17,12 @@ $welcome_text = fread($welcome_file, $size);
 echo $welcome_text;
 //then close once done
 fclose($welcome_file);
+
+
+//open file my bands in write mode
+$bands_file = fopen("my-bands.txt", "w");
+//use fwrite function to write the name of favorite band
+fwrite($bands_file, "Still Woozy");
+//check out the file then close...
+fclose($bands_file);
+
